c6685ccca6694b3ce6370bac99e2edd22aa6e502
Tomasz Iwanicki <tiko35@o2.pl>
2023-10-03 20:49:44 +0200

    Revert "#18 Możliwość usuwania opisu szkolenia"
    This reverts commit a46353d1998158fc56a8885abc2f1e5769fa20fe.

--- src/main/java/pl/javastart/bootcamp/domain/admin/AdminTrainingDescriptionController.java
+++ src/main/java/pl/javastart/bootcamp/domain/admin/AdminTrainingDescriptionController.java
@@ -51,12 +51,6 @@ public class AdminTrainingDescriptionController {
         }
     }
 
-    @GetMapping("/{id}/usun")
-    public String deleteDescription(@PathVariable Long id) {
-        trainingDescriptionService.deleteById(id);
-        return "redirect:/admin/opisy";
-    }
-
     @PostMapping("/edytuj")
     public String editDescription(TrainingDescription trainingDescription) {
         trainingDescriptionService.update(trainingDescription);
